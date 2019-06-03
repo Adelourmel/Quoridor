dafd2cdb5a121e0ab1146e83c8e22756461ff23d
Remi Audoin <remi.audoin@gmail.com>
2019-06-03 22:22:04 +0200

    première ébauche de classes de Test unit

---
+++ src/test/PawnTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PawnTest {
+
+
+  @Test
+  public final void testPawn(){
+
+  }
+
+  @Test
+  public final void getPlayer(){
+    P
+  }
+}
---
+++ src/test/QuoridorSettingsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuoridorSettingsTest {
+
+
+  @Test
+  public final void testQuoridorSettings(){
+
+  }
+}
