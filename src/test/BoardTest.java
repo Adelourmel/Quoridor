33fd5207facb60ea08868fa4f4e21e3010bd19de
Adelourmel <a.delourmel35@gmail.com>
2019-06-05 09:32:26 +0200

    Debut test

---
+++ src/test/BoardTest.java
@@ -0,0 +1,20 @@
+package quoridor.test;
+import org.junit.Before;
+import org.junit.Test;
+import quoridor.model.*;
+
+import static org.junit.Assert.*;
+
+public class BoardTest {
+  public Board board;
+
+  @Test
+  public void testBoard() {
+
+  }
+
+
+  public Board getBoard() {
+    return this.board;
+  }
+}
---
+++ src/test/MoveCalculatorTest.java
@@ -0,0 +1,25 @@
+package quoridor.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+public class MoveCalculator {
+  private Player player1;
+  private Player player2;
+  private Board board;
+
+  @Before
+  private setup() {
+    this.board =   
+  }
+  private Board
+  @Test
+  testIsLegalPawn() {
+
+  }
+
+}
