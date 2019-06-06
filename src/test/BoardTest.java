e94c48327231bb624ba42398dc7f2bcd092f289d
Adelourmel <a.delourmel35@gmail.com>
2019-06-06 17:13:13 +0200

    board Test

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -7,14 +7,42 @@ import static org.junit.Assert.*;
 
 public class BoardTest {
   public Board board;
+  public Player player1;
+
+
+  @Before
+  public void setup() {
+
+    this.player1 =  new HumanPlayer();
+
+    this.board = new Board(11, player1, player1);
+
+  }
 
   @Test
   public void testBoard() {
+      assertNotNull(this.board);
+  }
+
+
+  @Test
+  public void testGetSize() {
+    assertSame(11, player1.getSize());
+  }
+
+
+  @Test
+  public void testSetSize() {
+    int tmp = (int)(Math.random() * 20;
+    board.setSize(tmp);
+    assertEquals(tmp, board.getSize());
+  }
+  
+  @Test
+  public void setNewMove() {
+    assertFalse(this.board.setNewMove(-1, -1, player1));
 
   }
 
 
-  public Board getBoard() {
-    return this.board;
-  }
 }
