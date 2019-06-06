e7eaf79886fb0c1c501c1facbc7cc1ccfc49e3fd
Remi Audoin <remi.audoin@gmail.com>
2019-06-06 17:40:43 +0200

    wallTest finito

---
+++ src/WallTest.java
@@ -0,0 +1,53 @@
+package quoridor.test;
+
+import quoridor.model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WallTest {
+
+  private Wall wall;
+  private Player player1;
+
+  @Before
+  public void setup() {
+
+    Board board;
+    this.player1 = new AIPlayer("Test1", Color.BLACK, board);
+    this.player2 =  new AIPlayer("Test2", Color.WHITE, board);
+    board = new Board(this.player1, this.player2);
+    this.wall = new Wall(1, 2, player1);
+  }
+
+  @Test
+  public final void testWall(){
+
+    assertNotNull("Instance non creee", this.wall);
+    assertSame(1, this.wall.getPosX());
+    assertSame(2, this.wall.getPosY());
+  }
+
+  @Test
+  public final void testGetPlayer(){
+    assertEquals(this.player1, this.wall.getPosX());
+  }
+
+  @Test
+  public final void testSetPlayer(){
+    this.wall = new Wall(1, 2, player1);
+    this.wall.setPlayer(player2);
+    assertEquals(this.player2, this.wall.getPlayer());
+  }
+
+  @Test
+  // TODO - Modifier le toString en fonction.
+  public final void testToString(){
+    assertEquals("", this.wall.toString());
+  }
+
+
+
+
+}
