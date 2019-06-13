0b8df21f182d1970ec0b06dc1df233ce833b3b4b
Adelourmel <a.delourmel35@gmail.com>
2019-06-13 11:51:46 +0200

    FR

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,7 +5,7 @@ public class Launcher {
 	 *
 	 * @param args
 	 */
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO - implement Launcher.main
 		System.out.println("Hello World");
 	}
