e5f12a9a43f091788532e6c571c4ae00912e9e8d
Adelourmel <a.delourmel35@gmail.com>
2019-06-15 19:46:05 +0200

    Suite GridPanel

--- src/view/GridPanel.java
+++ src/view/GridPanel.java
@@ -8,13 +8,13 @@ import java.awt.geom.*;
 
 public class GridPanel extends JPanel {
   private Square[][] grid;
-  private Shape[][] gridGUI;
-  private final double SIZESQUAREPAWN = 60;
-  private final double WIDTHWALL = 20;
+  private Rectangle[][] gridGUI;
+  private final int SIZESQUAREPAWN = 60;
+  private final int WIDTHWALL = 20;
 
   public GridPanel(Square[][] grid) {
     this.grid = grid;
-    this.gridGUI = new Shape[this.grid.length][this.grid.length];
+    this.gridGUI = new Rectangle[this.grid.length][this.grid.length];
     initShape();
   }
 
@@ -22,32 +22,42 @@ public class GridPanel extends JPanel {
     for (int i = 0 ; i < this.grid.length ; i++) {
       for (int p = 0 ; p < this.grid[i].length ; p++) {
         if (this.grid[p][i].getSquareType() == SquareType.WALL_ONLY) {
-          double posX = (i/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
-          double posY = (p/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
+          int posX = (i/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
+          int posY = (p/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
           if (i%2 == 0) {
             posY += this.SIZESQUAREPAWN;
-            this.gridGUI[p][i] = new Rectangle2D.Double(posX, posY, this.SIZESQUAREPAWN, this.WIDTHWALL);
+            this.gridGUI[p][i] = new Rectangle(posX, posY, this.SIZESQUAREPAWN, this.WIDTHWALL);
 
+          }
+          else if (p%2 == 0) {
+            posX += this.SIZESQUAREPAWN;
+            this.gridGUI[p][i] = new Rectangle(posX, posY, this.WIDTHWALL, this.SIZESQUAREPAWN);
           }
           else {
             posX += this.SIZESQUAREPAWN;
-            this.gridGUI[p][i] = new Rectangle2D.Double(posX, posY, this.WIDTHWALL, this.SIZESQUAREPAWN);
+            posY += this.SIZESQUAREPAWN;
+            this.gridGUI[p][i] = new Rectangle(posX, posY, this.WIDTHWALL, this.WIDTHWALL);
           }
+
         }
         else {
-          double posX = (i/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
-          double posY = (p/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
-          this.gridGUI[p][i] = new Rectangle2D.Double(posX, posY, this.SIZESQUAREPAWN, this.SIZESQUAREPAWN);
+          int posX = (i/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
+          int posY = (p/2)*(this.SIZESQUAREPAWN + this.WIDTHWALL);
+          this.gridGUI[p][i] = new Rectangle(posX, posY, this.SIZESQUAREPAWN, this.SIZESQUAREPAWN);
         }
       }
     }
+
   }
   protected void paintComponent(Graphics grphcs) {
     super.paintComponent(grphcs);
     Graphics2D g2d = (Graphics2D) grphcs;
     for (int i = 0 ; i < this.gridGUI.length ; i++) {
+      g2d.setColor(Color.RED);
       for (int j = 0 ; j < this.gridGUI[i].length ; j++) {
-        g2d.draw(this.gridGUI[j][i]);
+
+        g2d.fill(this.gridGUI[j][i]);
+        g2d.setColor(Color.BLUE);
       }
     }
   }
