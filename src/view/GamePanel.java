47bdcf074b334169be35a0cd5c8c557e194a3d31
Remi Audoin <remi.audoin@gmail.com>
2019-06-17 14:46:38 +0200

    GamePanel

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -9,6 +9,14 @@ public class GamePanel extends JPanel {
 
   private GridPanel gridPanel;
   private GUI state;
+  private JPanel player1Panel;
+  private JLabel player1Label;
+
+  private JPanel player2Panel;
+  private JLabel player2Label;
+  private JLabel wall1Label;
+  private JLabel wall2Label;
+  private final String PATH ="../img";
 
 
   public  GamePanel(Square[][] grid){
@@ -16,6 +24,44 @@ public class GamePanel extends JPanel {
     this.setLayout(new BorderLayout());
     this.gridPanel = new GridPanel(grid);
     this.add(this.gridPanel);
+
+
+    this.player1Panel = new JPanel();
+    this.player1Panel.setLayout(new BoxLayout(this.player1Panel, BoxLayout.Y_AXIS));
+    this.player1Panel.setBackground(new Color(90,219,255));
+    this.player1Label = new JLabel("Nom Joueur 1");
+
+    this.player1Label.setFont(new Font("Arial", Font.PLAIN, 40));
+    this.player1Panel.setAlignmentX(CENTER_ALIGNMENT);
+    this.player1Panel.add(this.player1Label);
+    this.player1Panel.add(new SpacePanel());
+    this.wall1Label = new JLabel("Barriere Restante : 8");
+    this.wall1Label.setFont(new Font("Arial", Font.PLAIN, 40));
+    this.wall1Label.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5, true));
+    this.player1Panel.add(this.wall1Label);
+
+
+
+    this.player2Panel = new JPanel();
+    this.player2Panel.setLayout(new BoxLayout(this.player2Panel, BoxLayout.Y_AXIS));
+    this.player2Panel.setBackground(new Color(90,219,255));
+    this.player2Label = new JLabel("Nom Joueur 2");
+    this.player2Label.setFont(new Font("Arial", Font.PLAIN, 40));
+    this.player2Label.setAlignmentY(CENTER_ALIGNMENT);
+    this.player2Panel.add(this.player2Label);
+    this.player2Panel.add(new SpacePanel());
+    this.wall2Label = new JLabel("Barriere Restante : 10");
+    this.wall2Label.setFont(new Font("Arial", Font.PLAIN, 40));
+    this.wall2Label.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5, true));
+    this.player2Panel.add(this.wall2Label);
+
+    this.add(this.player1Panel, BorderLayout.WEST);
+    this.add(this.player2Panel, BorderLayout.EAST);
+
+
+
+
+
 
 
 
